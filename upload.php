914e0c7be418ce030b2caa2144d4cff2c482e7b6
Labib Ali Syed <syedlabib19@gmail.com>
2018-02-21 12:41:31 +1100

    Dynamic Database Tables(According to CSV columns)
    Database Table columns is named as per CSV columns(First Line) and data
    is loaded to columns accordingly.
    
    Number of DB table’s columns is also defined by CSV columns, and is not
    fixed.

--- upload.php
+++ upload.php
@@ -52,16 +52,17 @@ if(file_exists($target_file)){
 }
 
 
-// Uploading file to target directory after type, and duplication check is been done.
+// Uploading file to target directory after type and duplication check is been done.
 
 if($uploadOK == 1){
 
     move_uploaded_file($_FILES["uploadFile"]["tmp_name"], $target_file );
 
-    echo "The file " . $fileName . " has been uploaded successfully.";
-
 
     loadCSVTODatabase();
+
+    echo "The file " . $fileName . " has been uploaded successfully.";
+
 }else{
 
     echo "Sorry, there was an error uploading file. Please try again.";
@@ -89,21 +90,125 @@ function loadCSVTODatabase(){
 
 
     //open uploaded csv file with read only mode
+
     $csvFile = fopen("$fileToRead", 'r');
 
 
-    //skip first line
-    fgetcsv($csvFile);
 
-    //parse data from csv file line by line
-    while(($line = fgetcsv($csvFile)) !== FALSE){
-    //insert member data into database
+    //getting first line of csv (For table columns)
 
-        $db->query("INSERT INTO Marks (COURSE, SID, ASSID, SID2, ASS2_SUB, LEADER, A2_TTL, OVERALL_COMMENT, UI_ABLE_2_ADD_ATHLETE_2_GAME, UI_ABLE_2_RUN_GAME_VIA_GUI, UI_DISPLAY_RESULT, UI_DISP_ATHLETE_SCORE, UI_FILE_HANDLE, FILE_LOAD, FILE_SAVE, FILE_RELOAD, CODING_STYLE, COMMENTS_JAVADOC, GOOD_USER_UI_BONUS
-    ) VALUES ('".$line[0]."','".$line[1]."','".$line[2]."','".$line[3]."','".$line[4]."','".$line[5]."','".$line[6]."','".$line[7]."','".$line[8]."','".$line[9]."','".$line[10]."','".$line[11]."','".$line[12]."','".$line[13]."','".$line[14]."','".$line[15]."','".$line[16]."','".$line[17]."','".$line[18]."')");
+    $columnName = fgetcsv($csvFile);
 
 
-}
+
+
+    //-------------------------------------------------------------------------------
+    //
+    //          Creation Of Database Table for Uploaded CSV
+    //
+    //  1. File name is parsed and stored into variable to use it to assign Name of table.
+    //  2. Only one column is inserted at this stage with ID column, other will be done in next stage while looping through $columnName variable.
+    //
+    //-----------------------------------------------------------------------------
+
+
+    //Parsing uploaded file name
+
+    $fileName = pathinfo($_FILES['uploadFile']['name']);
+
+    // Storing in into $tableName variable to use it for Database table name.
+
+    $tableName = $fileName[filename];
+
+    // Creating table for uploaded CSV file
+
+    $sql = "CREATE TABLE $tableName (
+    id INT (10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    $columnName[0] VARCHAR (10) NOT NULL
+
+    )";
+
+    // checking table is successfully created.
+
+    if (mysqli_query($db, $sql)) {
+        echo "Table MyGuests created successfully";
+    } else {
+        echo "Error creating table: " . mysqli_error($db);
+    }
+
+    //----------------------------------END------------------------------------------------------------------------------
+
+
+
+
+
+
+
+    //-------------------------------------------------------------------------------
+    //
+    //Updating the columns name of database table as per csv First Row
+    //
+    //
+    //-----------------------------------------------------------------------------
+
+
+    for($i=0; $i<=sizeof($columnName); $i++) {
+
+        // this variable is used to get the name of the previous column already in the database table.
+        $PreviousColumn = $i-1;
+        $update = "ALTER TABLE $tableName ADD $columnName[$i] VARCHAR (100) NOT NULL AFTER $columnName[$PreviousColumn]";
+        $db->query($update);
+    }
+
+    //----------------------------------END------------------------------------------------------------------------------
+
+
+
+
+
+
+    //-------------------------------------------------------------------------------
+    //           Inserting the data into table from CSV into the right columns
+    //
+    // 1. Each row data is stored in $line array variable.
+    // 2. Columns name is taken from $columnName array variable, which is incremented to load data into all columns of a row.
+    // 3. $idForTableUpdate variable is used to keep track of the row whose columns is getting updated, variable is incremented after one CSV file row is loaded to the database table.
+    // 4. IF/ELSE is used so that ID is assigned to a single row, and then other columns can be updated using that ID for tracking.
+    // 5. While assigning ID to a row, first column after ID is also populated with data.
+    //-----------------------------------------------------------------------------
+
+    $idForTableUpdate = 1;
+
+   // parse data from csv file line by line
+    while(($line = fgetcsv($csvFile)) !== FALSE) {
+        //insert member data into database
+
+
+        for ($i = 0; $i <= sizeof($columnName); $i++) {
+
+            // when $i = 0, ID is inserted to the column which will be used to update data into all columns of that ID's row.
+
+            if($i >= 1){
+                $db->query("UPDATE $tableName SET  $columnName[$i]= '" . $line[$i] . "' WHERE id=$idForTableUpdate");
+            }else {
+
+                // First column after ID column is also inserted in the stage.
+
+                $db->query("INSERT INTO $tableName  ($columnName[$i]) VALUES ('" . $line[$i] . "')");
+            }
+
+
+        }
+
+        // After all columns of specific ID is updated with data, ID is incremented to load data in next row
+        $idForTableUpdate++;
+
+    }
+
+    //----------------------------------END------------------------------------------------------------------------------
+
+
+
 
 }
 
